0e305e82626bb9c27d6e45cd9f0cf29cab57a996
Rytisok <mikrytis@gmail.com>
2023-05-27 11:33:42 +0300

    extensions fix

--- FlowerShop/Extensions/ApplicationServiceExtension.cs
+++ FlowerShop/Extensions/ApplicationServiceExtension.cs
@@ -1,7 +1,55 @@
+using FlowerShop.Interfaces;
+using FlowerShop.Repositories;
+using FlowerShop.Services;
+using Microsoft.OpenApi.Models;
+
 namespace FlowerShop.Extensions
 {
     public static class ApplicationServiceExtension
     {
-        
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnetClaimAuthorization", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference= new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            new string[]{}
+                        }
+                    });
+            });
+
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IItemsRepository, ItemsRepository>();
+            services.AddScoped<IMerchandiseRepository, MerchandiseRepository>();
+            services.AddScoped<IMerchandiseCategoryRepository, MerchandiseCategoryRepository>();
+            services.AddScoped<IOrdersRepository, OrdersRepository>();
+
+            return services;
+        }
     }
 }
--- FlowerShop/Program.cs
+++ FlowerShop/Program.cs
@@ -19,13 +19,6 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddScoped<ITestRepository, TestRepository>(); //Temporary service to test database functionality
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IItemsRepository, ItemsRepository>();
-builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
-builder.Services.AddScoped<IMerchandiseRepository, MerchandiseRepository>();    
-builder.Services.AddScoped<IMerchandiseCategoryRepository, MerchandiseCategoryRepository>();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
